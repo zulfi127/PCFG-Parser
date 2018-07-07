375945263e7b7d62530369012cbb5a0bbafea709
zulfi127 <40912542+zulfi127@users.noreply.github.com>
2018-07-07 15:51:29 +0200

    Update parser_project_parent.py

--- parser_project_parent.py
+++ parser_project_parent.py
@@ -1,7 +1,3 @@
-# Full Name: Zulipiye Yusupujiang
-# Date: April 4th, 2018
-# Assignment: Syntactic Parsing Assignment 4: Individual Project
-# Reference: Course Slides for 2018 Syntactic Parsing Course
 
 
 """
